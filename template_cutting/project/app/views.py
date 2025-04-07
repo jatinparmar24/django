4e84f12650d6b61230d057181ae432577280750e
jatinparmar24 <jatinparmar2424@gmail.com>
2025-04-07 22:57:13 +0530

    django boiler plate

--- template_cutting/project/app/views.py
+++ template_cutting/project/app/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render('request','home.html')
+    return render('request','loading.html')
+
+def About(request):
+    return render(request,'About.html')
--- template_cutting/project/project/urls.py
+++ template_cutting/project/project/urls.py
@@ -20,5 +20,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('About/' ,views.About,name='About')
 ]
