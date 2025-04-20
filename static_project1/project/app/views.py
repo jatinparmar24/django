d06d21af18916571296b26073573814295b97f59
jatinparmar24 <jatinparmar2424@gmail.com>
2025-04-20 16:01:27 +0530

    django p login and registration start

--- static_project1/project/app/views.py
+++ static_project1/project/app/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def about(request):
     return render(request,'about.html')
+
+def service(request):
+    return render(request,'service.html')
--- static_project1/project/project/urls.py
+++ static_project1/project/project/urls.py
@@ -24,7 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-    path('about/',views.about,name='about')
+    path('about/',views.about,name='about'),
+    path('service/',views.service,name='service'),
 
     
 ] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
