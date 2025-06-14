ab7910cf08c6e6d40a92323fbd152d81014d7966
jatinparmar24 <jatinparmar2424@gmail.com>
2025-06-14 21:50:45 +0530

    integration with react done

--- integratewithreact/project/app/models.py
+++ integratewithreact/project/app/models.py
@@ -2,18 +2,14 @@ from django.db import models
 
 class Registrations(models.Model):
     name = models.CharField(max_length=100)
-    mode = models.CharField(max_length=100,blank=True)
-    country = models.CharField(max_length=100,blank=True)
+    country = models.CharField(max_length=100, blank=True)
     age = models.IntegerField()
-    contact = models.IntegerField(unique=True,blank=True)
-    day = models.IntegerField(blank=True)
-    person = models.IntegerField(blank= True)
-    price = models.IntegerField(blank=True)
-    date = models.DateField(default=True ,blank=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-   
-    
+    contact = models.IntegerField(unique=True, blank=True, null=True)
+    day = models.IntegerField(blank=True, null=True)
+    person = models.IntegerField(blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    # Removed email and password fields
 
     def __str__(self):
-        return self.email
+        return self.name
